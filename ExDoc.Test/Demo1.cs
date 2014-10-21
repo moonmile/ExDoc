47502b3352c405c4bea350b58eb76e6804a19da8
Tomoaki Masuda <masuda@moonmile.net>
2014-10-21 18:41:37 +0900

    ExElement の / のテストを追加

---
+++ ExDoc.Test/Demo1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Linq;
+using Moonmile.ExDoc;
+
+namespace ExDoc.Test
+{
+    [TestClass]
+    public class TestExDocument
+    {
+
+        /// <summary>
+        /// Parse Xml string
+        /// </summary>
+        [TestMethod]
+        public void TestLoad()
+        {
+            var xml = "<root><person name='tomoaki'>masuda</person></root>";
+
+            var doc = ExDocument.LoadXml(xml);
+            Assert.AreEqual("root", doc.Root.Name);
+            // seek element
+            ExElement el = doc * "person";
+            Assert.AreEqual("person", el.Name );
+            Assert.AreEqual("masuda", el.Value);
+            string val = el % "name";
+            Assert.AreEqual("tomoaki", val);
+        }
+
+        /// <summary>
+        /// Query Xml
+        /// </summary>
+        [TestMethod]
+        public void TestQuery()
+        {
+            var xml = "<root>" + 
+                "<person id='1'>masuda</person>" +  
+                "<person id='2'>tomoaki</person>" +  
+                "<person id='3'>yumi</person>" +  
+                "</root>";
+
+            var doc = ExDocument.LoadXml(xml);
+
+            // query element
+            ExElement el = doc * "person" % "id" == "2";
+            Assert.AreEqual("tomoaki", el.Value);
+            // query elements
+            ExElements els = doc * "person";
+            Assert.AreEqual(3, els.Count);
+            Assert.AreEqual("masuda", els[0].Value);
+            Assert.AreEqual("tomoaki", els[1].Value);
+            Assert.AreEqual("yumi", els[2].Value);
+        }
+
+        /// <summary>
+        /// auto cast
+        /// </summary>
+        [TestMethod]
+        public void TestAutoCast()
+        {
+            var xml = "<root>" +
+                "<person id='1' age='20'>masuda</person>" +
+                "<person id='2' age='30'>tomoaki</person>" +
+                "<person id='3' age='40'>yumi</person>" +
+                "</root>";
+
+            var doc = ExDocument.LoadXml(xml);
+
+            // query elements
+            ExElement el = doc * "person" % "id" == 2;
+            Assert.AreEqual(30, el % "age" );    // get attribute number
+            Assert.AreEqual("tomoaki", el);     // get element value string
+        }
+    
+    }
+}
---
+++ ExDoc.Test/TestExElement.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Linq;
+using Moonmile.ExDoc;
+
+namespace ExDoc.Test
+{
+    [TestClass]
+    public class TestExElement
+    {
+        [TestMethod]
+        public void TestQuery1()
+        {
+            var xml = @"
+<root> 
+    <person id='1'>masuda</person>
+    <person id='2'>
+        <child>kaho</child>
+        <child>kazuhisa</child>
+    </person> 
+    <person id='3'>yumi</person>
+</root>";
+
+            var doc = ExDocument.LoadXml(xml);
+
+            ExElement el = doc * "person";
+            Assert.AreEqual("masuda", el.Value );
+
+            ExElement el2 = doc / "person";
+            Assert.AreEqual("masuda", el2.Value);
+        }
+
+        [TestMethod]
+        public void TestQuery2()
+        {
+            var xml = @"
+<root> 
+    <person id='1'>masuda</person>
+    <person id='2'>
+        <child>kaho</child>
+        <child>kazuhisa</child>
+    </person> 
+    <person id='3'>yumi</person>
+</root>";
+
+            var doc = ExDocument.LoadXml(xml);
+
+            /// 子孫を調べるとある
+            ExElement el = doc * "child";
+            Assert.AreEqual("kaho", el.Value);
+            /// 子要素だけだと無い
+            ExElement el2 = doc / "child";
+            Assert.AreEqual("", el2.Value);
+        }
+    }
+}
