12a39b9717d5cba804e9ca5d67cdecd6f0b2c961
Tomoaki Masuda <masuda@moonmile.net>
2014-10-22 10:58:29 +0900

    ExDocFSharpTest.fs の入れ忘れ

---
+++ ExDocFSharp.Test/ExDocFSharpTest.fs
@@ -0,0 +1,28 @@
+﻿module ExDocFSharp.Test
+open FsUnit
+open NUnit.Framework
+open Moonmile.ExDoc
+
+[<TestFixture>]
+type ``Demo Fsharp test``() =
+    
+    [<Test>]
+    member this.``loading test``() =
+        
+            let xml = """
+<root>
+    <person name='tomoaki'>masuda</person>
+</root>
+"""
+            let doc = ExDocument.LoadXml(xml)
+            doc.Root.Name |> should equal "root"
+            // seek element
+            let el:ExElement = doc * "person" |> ExElements.op_Implicit
+            Assert.AreEqual("person", el.Name );
+            Assert.AreEqual("masuda", el.Value);
+            let v = el % "name"
+            Assert.AreEqual("tomoaki", v.Value);
+
+
+
+
